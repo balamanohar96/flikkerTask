75e78144355780ef4756787b6875194a4e715f9e
balamanohar96 <balamanohar.yeminedi@gmail.com>
2022-10-31 18:48:01 +0530

    test

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Images from "./components/Images"
+
+function App() {
+  return (
+    <div>
+      <Images></Images>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Images.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from "react";
+import "./Images.css";
+
+const Images = () => {
+  const [imagesList, setImagesList] = useState([]);
+
+  useEffect(() => {
+    const task = async () => {
+      const res = await fetch(
+        "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"
+      );
+      const data = await res.json();
+      console.log(data.photos.photo);
+      setImagesList(data.photos.photo);
+    };
+    task();
+  }, []);
+
+  return (
+    <div className="app-container">
+      <h1 className="heading">Images</h1>
+      <div className="images-container">
+        {imagesList.map((each, i) => {
+          return (
+            <div className="image-container" key={i}>
+              <img
+                src={`https://farm${each.farm}.staticflickr.com/${each.server}/${each.id}_${each.secret}.jpg`}
+                className="image"
+                alt="shopping-images"
+              />
+              <p className="title">{each.title}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default Images;
