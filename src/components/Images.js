ac14577927a921c7831d18e1b404b30b811a3b67
balamanohar96 <balamanohar.yeminedi@gmail.com>
2022-11-02 12:09:49 +0530

    test

--- src/components/Images.js
+++ src/components/Images.js
@@ -10,7 +10,6 @@ const Images = () => {
         "https://www.flickr.com/services/rest/?method=flickr.galleries.getPhotos&api_key=f9736f4d370f9c7115a952951b506569&gallery_id=66911286-72157647277042064&format=json&nojsoncallback=1"
       );
       const data = await res.json();
-      console.log(data.photos.photo);
       setImagesList(data.photos.photo);
     };
     task();
